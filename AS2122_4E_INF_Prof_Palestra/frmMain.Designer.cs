41a48b1e120a56b58de762e20eeca4ea896b0191
Filippo Albertini <filippo@filippoalbertini.it>
2022-03-09 10:02:06 +0100

    Inizio impostazione UI esercizio con inserimento persone e scelta mesi

--- AS2122_4E_INF_Prof_Palestra/frmMain.Designer.cs
+++ AS2122_4E_INF_Prof_Palestra/frmMain.Designer.cs
@@ -29,20 +29,67 @@ namespace AS2122_4E_INF_Prof_Palestra
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNumeroPersone = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbMesi = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
-            // Form1
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Numero persone";
+            // 
+            // txtNumeroPersone
+            // 
+            this.txtNumeroPersone.Location = new System.Drawing.Point(177, 39);
+            this.txtNumeroPersone.Name = "txtNumeroPersone";
+            this.txtNumeroPersone.Size = new System.Drawing.Size(105, 27);
+            this.txtNumeroPersone.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Mesi";
+            // 
+            // cmbMesi
+            // 
+            this.cmbMesi.FormattingEnabled = true;
+            this.cmbMesi.Location = new System.Drawing.Point(177, 102);
+            this.cmbMesi.Name = "cmbMesi";
+            this.cmbMesi.Size = new System.Drawing.Size(151, 28);
+            this.cmbMesi.TabIndex = 2;
+            // 
+            // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
+            this.Controls.Add(this.cmbMesi);
+            this.Controls.Add(this.txtNumeroPersone);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmMain";
             this.Text = "Prof; Lab. 2.2 4E; 09/03/22; gestione palestra";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNumeroPersone;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbMesi;
     }
 }
 
